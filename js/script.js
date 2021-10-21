a890b2235b5771b14dcd6d178bd5f1e441dde123
Maurizio <proiettomaurizio97@gmail.com>
2021-10-21 15:20:33 +0200

    push

--- js/script.js
+++ js/script.js
@@ -6,20 +6,22 @@ va applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
 
-const ticketCost = document.getElementById(ticket-cost);
-// I ask the user the km he has to travel        (chiedo all'utente i km che deve percorrere)
+const ticketCost = document.getElementById("ticket-cost").innerHTML("ciao mondo")
+
+// I ask the user the km he has to travel   (chiedo all'utente i km che deve percorrere)
+const km = prompt("How many km do you have to travel?");
+console.log(km)
+
+// i ask the user how old he is  (chiedo all'utente quanti anni ha)
 
 
-// i ask the user how old he is (chiedo all'utente quanti anni ha)
+// i calculate how much the routes costs whitout discounts    (calcolo quanto costa la tratta senza sconti)
 
 
-// i calculate how much the routes costs whitout discounts  (calcolo quanto costa la tratta senza sconti)
+// if the user is under 18 years i apply a 20% discount   (se l'utente ha meno di 18 anni applico uno sconto del 20%)
 
 
-// if the user is under 18 years i apply a 20% discount(se l'utente ha meno di 18 anni applico uno sconto del 20%)
+// if the user is over 65 years i apply a 40% discount   (se l'utente ha più di 65 anni applico uno sconto del 40%)
 
 
-// if the user is over 65 years i apply a 40% discount (se l'utente ha più di 65 anni applico uno sconto del 40%)
-
-
-// i tell the user the cost of the ticket (dico all'utente il costo del biglietto)
+// i tell the user the cost of the ticket   (dico all'utente il costo del biglietto)
