32b9b95a310f18fa5ef8c2f0ec072b96fb027bcd
Maurizio <proiettomaurizio97@gmail.com>
2021-10-21 15:07:15 +0200

    i divided the delivery into steps

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/* Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+
+const ticketCost = document.getElementById(ticket-cost);
+// I ask the user the km he has to travel        (chiedo all'utente i km che deve percorrere)
+
+
+// i ask the user how old he is (chiedo all'utente quanti anni ha)
+
+
+// i calculate how much the routes costs whitout discounts  (calcolo quanto costa la tratta senza sconti)
+
+
+// if the user is under 18 years i apply a 20% discount(se l'utente ha meno di 18 anni applico uno sconto del 20%)
+
+
+// if the user is over 65 years i apply a 40% discount (se l'utente ha più di 65 anni applico uno sconto del 40%)
+
+
+// i tell the user the cost of the ticket (dico all'utente il costo del biglietto)
