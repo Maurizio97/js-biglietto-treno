94c4292d7e5ee7f93374c897b983abb96df57d0f
Maurizio <proiettomaurizio97@gmail.com>
2021-10-21 16:18:06 +0200

    step 2, 3  and 4 complete

--- js/script.js
+++ js/script.js
@@ -7,22 +7,35 @@ L’output del prezzo finale va messo fuori in forma umana (con massimo due deci
 
 
 const ticketCost = document.getElementById("ticket-cost")
+ticketCost.innerHTML = "ticket cost is:";
 
 // I ask the user the km he has to travel   (chiedo all'utente i km che deve percorrere)
 const km = parseInt(prompt("How many km do you have to travel?"));
 console.log("km", km);
 
 // i ask the user how old he is  (chiedo all'utente quanti anni ha)
-const age = parseInt(prompt("How old are you?"))
-console.log("user age", yearsOld)
+const age = parseInt(prompt("How old are you?"));
+console.log("user age", age);
 
 // i calculate how much the routes costs whitout discounts    (calcolo quanto costa la tratta senza sconti)
-
+let baseCost = km * 0.21;
+console.log("base cost", baseCost);
 
 // if the user is under 18 years i apply a 20% discount   (se l'utente ha meno di 18 anni applico uno sconto del 20%)
+let costUnder18;
+if (age < 18) {
+    costUnder18 = (baseCost * 20) / 100;
+    ticketCost 
+} 
 
+console.log("20% discount", costUnder18);
 
 // if the user is over 65 years i apply a 40% discount   (se l'utente ha più di 65 anni applico uno sconto del 40%)
+let costOver65;
+if (age > 65) {
+    costOver65 = baseCost * 0.40;
+}
 
+console.log("40% discount", costOver65);
 
 // i tell the user the cost of the ticket   (dico all'utente il costo del biglietto)
